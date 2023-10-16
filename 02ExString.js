93ca0044d09ffa8ada9acb4f897d88ee29e8379d
AshokSd493 <147791806+AshokSd493@users.noreply.github.com>
2023-10-16 05:48:27 +0000

    test

---
+++ 02ExString.js
@@ -0,0 +1,39 @@
+const name = "Ashok"
+
+const dept = "QA"
+
+console.log(`Hello my name is ${name} and I work as ${dept}`)
+
+const newName = new String("Ashok")
+
+// console.log("Hello my name is "+name+" and I work as "+dept);
+
+ 
+
+// console.log(`Hello my name is ${name} and I work as ${dept}`)
+
+ 
+
+const newName = new String("Kumar")
+
+ 
+
+// console.log(typeof name);
+
+// console.log(typeof newName);
+
+ 
+
+console.log(newName[0]);
+
+console.log(newName.__proto__);
+
+console.log(newName.length);
+
+console.log(newName.toUpperCase());
+
+ 
+
+const newString = newName.substring(0,5)
+
+console.log(newString);
---
+++ 02String.js
@@ -0,0 +1,24 @@
+const stringVal= "Piyush"
+
+const nullValue = null;
+
+let notDefined;
+let userOne ={
+
+    email:"someone@google.com",
+
+    phone:"9922001100"
+
+}
+
+ 
+
+let userTwo = userOne
+
+ 
+
+userTwo.email = "piyush@google.com"
+
+console.log(userOne.email);
+
+console.log(userTwo.email);
